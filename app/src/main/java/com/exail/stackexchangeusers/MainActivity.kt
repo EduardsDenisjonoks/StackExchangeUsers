e12d8af39262d251fc6a6aa2f3c886542b0614a4
Eduards Denisjonoks <ed@wearesurge.co>
2020-08-12 16:58:26 +0100

    + Initial push

---
+++ app/src/main/java/com/exail/stackexchangeusers/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.exail.stackexchangeusers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
